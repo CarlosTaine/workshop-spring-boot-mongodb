4a1caf7e0f6baa0a74970b29984f9e0be0585fd9
CarlosTaine <taine.goncalves.s@gmail.com>
2022-06-28 23:01:11 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/carlostaine/workshopmogo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.carlostaine.workshopmogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
